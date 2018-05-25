53a5eaa9bdb138ee544f82b7e1bf92d7d6639eab
Ben CP <ben.coole-probert@syentium.com>
2018-05-25 17:01:23 +0100

    Added Object recognition for images and video
    Added feature matching using SIFT with seperate images, or a video stream

--- SIFTDemo/SIFT.Designer.cs
+++ SIFTDemo/SIFT.Designer.cs
@@ -29,56 +29,51 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnSelectImage = new System.Windows.Forms.Button();
-            this.btnProcess = new System.Windows.Forms.Button();
-            this.ibx = new Emgu.CV.UI.ImageBox();
-            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.btnSelectImages = new System.Windows.Forms.Button();
+            this.ibxModel = new Emgu.CV.UI.ImageBox();
+            this.ibxResult = new Emgu.CV.UI.ImageBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.btnRecord = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.ibx)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.rdoImages = new System.Windows.Forms.RadioButton();
+            this.rdoVideo = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxModel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxResult)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnSelectImage
+            // btnSelectImages
             // 
-            this.btnSelectImage.Location = new System.Drawing.Point(3, 495);
-            this.btnSelectImage.Name = "btnSelectImage";
-            this.btnSelectImage.Size = new System.Drawing.Size(654, 48);
-            this.btnSelectImage.TabIndex = 0;
-            this.btnSelectImage.Text = "Select Image";
-            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImages.Location = new System.Drawing.Point(3, 711);
+            this.btnSelectImages.Name = "btnSelectImages";
+            this.btnSelectImages.Size = new System.Drawing.Size(1614, 48);
+            this.btnSelectImages.TabIndex = 0;
+            this.btnSelectImages.Text = "Select Images";
+            this.btnSelectImages.UseVisualStyleBackColor = true;
+            this.btnSelectImages.Click += new System.EventHandler(this.btnSelectImages_Click);
             // 
-            // btnProcess
+            // ibxModel
             // 
-            this.btnProcess.Location = new System.Drawing.Point(663, 495);
-            this.btnProcess.Name = "btnProcess";
-            this.btnProcess.Size = new System.Drawing.Size(645, 48);
-            this.btnProcess.TabIndex = 1;
-            this.btnProcess.Text = "Process Image";
-            this.btnProcess.UseVisualStyleBackColor = true;
+            this.ibxModel.Location = new System.Drawing.Point(3, 53);
+            this.ibxModel.Name = "ibxModel";
+            this.ibxModel.Size = new System.Drawing.Size(798, 593);
+            this.ibxModel.TabIndex = 2;
+            this.ibxModel.TabStop = false;
             // 
-            // ibx
+            // ibxResult
             // 
-            this.ibx.Location = new System.Drawing.Point(3, 53);
-            this.ibx.Name = "ibx";
-            this.ibx.Size = new System.Drawing.Size(654, 436);
-            this.ibx.TabIndex = 2;
-            this.ibx.TabStop = false;
-            // 
-            // imageBox2
-            // 
-            this.imageBox2.Location = new System.Drawing.Point(663, 53);
-            this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(645, 436);
-            this.imageBox2.TabIndex = 3;
-            this.imageBox2.TabStop = false;
+            this.ibxResult.Location = new System.Drawing.Point(807, 53);
+            this.ibxResult.Name = "ibxResult";
+            this.ibxResult.Size = new System.Drawing.Size(810, 593);
+            this.ibxResult.TabIndex = 3;
+            this.ibxResult.TabStop = false;
             // 
             // flowLayoutPanel1
             // 
@@ -87,30 +82,22 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.Controls.Add(this.panel2);
             this.flowLayoutPanel1.Controls.Add(this.panel1);
-            this.flowLayoutPanel1.Controls.Add(this.ibx);
-            this.flowLayoutPanel1.Controls.Add(this.imageBox2);
-            this.flowLayoutPanel1.Controls.Add(this.btnSelectImage);
-            this.flowLayoutPanel1.Controls.Add(this.btnProcess);
+            this.flowLayoutPanel1.Controls.Add(this.ibxModel);
+            this.flowLayoutPanel1.Controls.Add(this.ibxResult);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.btnSelectImages);
             this.flowLayoutPanel1.Controls.Add(this.btnRecord);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1311, 669);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1711, 945);
             this.flowLayoutPanel1.TabIndex = 4;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(663, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(645, 44);
-            this.panel1.TabIndex = 4;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(3, 3);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(654, 44);
+            this.panel2.Size = new System.Drawing.Size(798, 44);
             this.panel2.TabIndex = 5;
             // 
             // label1
@@ -126,10 +113,18 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Model Image:";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(807, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(810, 44);
+            this.panel1.TabIndex = 4;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(267, 17);
+            this.label2.Location = new System.Drawing.Point(353, 17);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(121, 17);
@@ -138,43 +133,83 @@
             // 
             // btnRecord
             // 
-            this.btnRecord.Location = new System.Drawing.Point(3, 549);
+            this.btnRecord.Location = new System.Drawing.Point(3, 765);
             this.btnRecord.Name = "btnRecord";
-            this.btnRecord.Size = new System.Drawing.Size(1305, 48);
+            this.btnRecord.Size = new System.Drawing.Size(1614, 48);
             this.btnRecord.TabIndex = 6;
-            this.btnRecord.Text = "Start Recording";
+            this.btnRecord.Text = "Start Capture";
             this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.rdoVideo);
+            this.panel3.Controls.Add(this.rdoImages);
+            this.panel3.Location = new System.Drawing.Point(3, 652);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1614, 53);
+            this.panel3.TabIndex = 7;
+            // 
+            // rdoImages
+            // 
+            this.rdoImages.AutoSize = true;
+            this.rdoImages.Location = new System.Drawing.Point(289, 17);
+            this.rdoImages.Name = "rdoImages";
+            this.rdoImages.Size = new System.Drawing.Size(74, 21);
+            this.rdoImages.TabIndex = 0;
+            this.rdoImages.TabStop = true;
+            this.rdoImages.Text = "Images";
+            this.rdoImages.UseVisualStyleBackColor = true;
+            this.rdoImages.CheckedChanged += new System.EventHandler(this.rdoImages_CheckedChanged);
+            // 
+            // rdoVideo
+            // 
+            this.rdoVideo.AutoSize = true;
+            this.rdoVideo.Location = new System.Drawing.Point(1160, 17);
+            this.rdoVideo.Name = "rdoVideo";
+            this.rdoVideo.Size = new System.Drawing.Size(65, 21);
+            this.rdoVideo.TabIndex = 1;
+            this.rdoVideo.TabStop = true;
+            this.rdoVideo.Text = "Video";
+            this.rdoVideo.UseVisualStyleBackColor = true;
+            this.rdoVideo.CheckedChanged += new System.EventHandler(this.rdoVideo_CheckedChanged);
             // 
             // SIFT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1335, 693);
+            this.ClientSize = new System.Drawing.Size(1735, 969);
             this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "SIFT";
             this.Text = "SIFT";
-            ((System.ComponentModel.ISupportInitialize)(this.ibx)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.ibxModel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibxResult)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnSelectImage;
-        private System.Windows.Forms.Button btnProcess;
-        private Emgu.CV.UI.ImageBox ibx;
-        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button btnSelectImages;
+        private Emgu.CV.UI.ImageBox ibxModel;
+        private Emgu.CV.UI.ImageBox ibxResult;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RadioButton rdoVideo;
+        private System.Windows.Forms.RadioButton rdoImages;
     }
 }
--- SIFTDemo/SIFT.cs
+++ SIFTDemo/SIFT.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
@@ -9,11 +12,122 @@ namespace SIFTDemo
 {
     public partial class SIFT : Form
     {
+        private VideoCapture _capture = null;
+        private bool _captureInProgress;
+        private Mat _frame;
+        private string ModelImage, OtherImage;
+        
+
         public SIFT()
         {
             InitializeComponent();
+            rdoImages.Checked = true;
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new VideoCapture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            _frame = new Mat();
+        }
 
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+                ibxModel.Image = _frame;
+                long matchTime;
+                Image<Bgr, Byte> rstImg = new Image<Bgr, Byte>(ibxModel.Image.Bitmap);
+                using (Mat modelImage = rstImg.Mat)
+                using (Mat observedImage = CvInvoke.Imread(ModelImage, ImreadModes.Color))
+                {
+                    Mat result = FeatureMatch.Draw(modelImage, observedImage, out matchTime);
+                    ibxResult.Image = result;
+                }
+            }
+        }
 
+        private void ReleaseData()
+        {
+            if (_capture != null)
+                _capture.Dispose();
+        }
+
+        private string getImage(string Image)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = $@"Select a {Image} Image";
+            DialogResult result = ofd.ShowDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "Image files (*.jpg, *.jpeg, *.png, *.tif, *.tiff) | *.jpg; *.jpeg; *.png; *.tif; *.tiff;";
+            if (result == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            if(result == DialogResult.Cancel)
+            {
+                getImage(Image);
+            }
+            return null;
+        }
+
+        private void btnSelectImages_Click(object sender, EventArgs e)
+        {
+            ModelImage = getImage("Model");
+            ibxModel.Image = CvInvoke.Imread(ModelImage, ImreadModes.Color);
+            OtherImage = getImage("Test");
+
+            if(!String.IsNullOrEmpty(ModelImage) && !String.IsNullOrEmpty(OtherImage))
+            {
+                long matchTime;
+                using (Mat modelImage = CvInvoke.Imread(ModelImage, ImreadModes.Color))
+                using (Mat observedImage = CvInvoke.Imread(OtherImage, ImreadModes.Color))
+                {
+                    Mat result = FeatureMatch.Draw(modelImage, observedImage, out matchTime);
+                    ibxResult.Image = result;
+                }
+            }
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ModelImage))
+            {
+                ModelImage = getImage("Model");
+            }
+
+            if (_capture != null)
+            {
+                if (_captureInProgress)
+                {  //stop the capture
+                    btnRecord.Text = "Start Capture";
+                    _capture.Pause();
+                }
+                else
+                {
+                    //start the capture
+                    btnRecord.Text = "Stop Capturing";
+                    _capture.Start();
+                }
+                _captureInProgress = !_captureInProgress;
+            }
+        }
+
+        private void rdoImages_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSelectImages.Visible = true;
+            btnRecord.Visible = false;
+        }
+
+        private void rdoVideo_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSelectImages.Visible = false;
+            btnRecord.Visible = true;
         }
     }
 }
